e47eb136f7cfb810c19d3afcaf681dc278731dc9
jarick <jarick@jarick-HP-ProBook-4535s>
2014-03-21 20:51:12 +0300

    Safe validator

--- src/BX/Validator/Manager/Safe.php
+++ src/BX/Validator/Manager/Safe.php
@@ -1,1 +1,17 @@
-<?php
+<?php namespace BX\Validator\Manager;
+
+class Safe extends BaseValidator
+{
+	/**
+	 * Validate
+	 * @param string $key
+	 * @param string $value
+	 * @param string $label
+	 * @param string $fields
+	 * @return boolean
+	 */
+	public function validate($key,$value,$label,&$fields)
+	{
+		return true;
+	}
+}
